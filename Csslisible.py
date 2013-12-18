a06c3b54ab7f4d2dfd49570aad44ba063409c325
Baptiste Millou <baptiste@smoothie-creative.com>
2013-12-18 13:59:18 -0500

    Fix incorrect settings loading.
    Signed-off-by: Baptiste Millou <baptiste@smoothie-creative.com>

--- Csslisible.py
+++ Csslisible.py
@@ -3,10 +3,12 @@ import sublime
 import sublime_plugin
 import urllib
 
-settings = sublime.load_settings('Csslisible.sublime-settings')
-
 
 class CsslisibleCommand(sublime_plugin.TextCommand):
+    def __init__(self, *args, **kwargs):
+        super(CsslisibleCommand, self).__init__(*args, **kwargs)
+        self.settings = sublime.load_settings('Csslisible.sublime-settings')
+
     def run(self, edit):
         # We check for selections if no selections are present we run the API
         # on the whole file
@@ -17,7 +19,7 @@ class CsslisibleCommand(sublime_plugin.TextCommand):
 
         for region in regions:
             original = self.view.substr(region)
-            result = cssLisibleApiCall(original)
+            result = self.cssLisibleApiCall(original)
             if result:
                 result = self.normalize_line_endings(result)
                 self.view.replace(edit, region, result)
@@ -31,31 +33,30 @@ class CsslisibleCommand(sublime_plugin.TextCommand):
             string = string.replace('\n', '\r')
         return string
 
+    def cssLisibleApiCall(self, string):
+        url = urllib.parse.urlparse(self.settings.get('csslisible_URL'))
+        data = {
+            'api': '1',
+            'clean_css': string,
 
-def cssLisibleApiCall(string):
-    url = urllib.parse.urlparse(settings.get('csslisible_URL'))
-    data = {
-        'api': '1',
-        'clean_css': string,
-
-        'distance_selecteurs': settings.get('distance_selecteurs'),
-        'type_indentation': settings.get('type_indentation'),
-        'type_separateur': settings.get('type_separateur'),
-        'selecteurs_multiples_separes': settings.get('selecteurs_multiples_separes'),
-        'valeurs_multiples_separees': settings.get('valeurs_multiples_separees'),
-        'hex_colors_format': settings.get('hex_colors_format'),
-        'colors_format': settings.get('colors_format'),
-        'raccourcir_valeurs': settings.get('raccourcir_valeurs'),
-    }
-    params = urllib.parse.urlencode(data)
-    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-    try:
-        conn = http.client.HTTPConnection(url.netloc, timeout=5)
-        conn.request("POST", url.path, params, headers)
-    except http.client.HTTPException as e:
-        sublime.error_message('%s: HTTP error %s contacting API' % (__name__, str(e)))
-    else:
-        response = conn.getresponse().read()
-        conn.close()
-        return str(response, encoding='UTF-8')
-    return False
+            'distance_selecteurs': self.settings.get('distance_selecteurs'),
+            'type_indentation': self.settings.get('type_indentation'),
+            'type_separateur': self.settings.get('type_separateur'),
+            'selecteurs_multiples_separes': self.settings.get('selecteurs_multiples_separes'),
+            'valeurs_multiples_separees': self.settings.get('valeurs_multiples_separees'),
+            'hex_colors_format': self.settings.get('hex_colors_format'),
+            'colors_format': self.settings.get('colors_format'),
+            'raccourcir_valeurs': self.settings.get('raccourcir_valeurs'),
+        }
+        params = urllib.parse.urlencode(data)
+        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+        try:
+            conn = http.client.HTTPConnection(url.netloc, timeout=5)
+            conn.request("POST", url.path, params, headers)
+        except http.client.HTTPException as e:
+            sublime.error_message('%s: HTTP error %s contacting API' % (__name__, str(e)))
+        else:
+            response = conn.getresponse().read()
+            conn.close()
+            return str(response, encoding='UTF-8')
+        return False
